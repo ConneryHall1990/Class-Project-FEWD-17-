d6c68963570148550a234d10cd5af26bdbaf4df4
ConneryHall1990 <conneryhall@gmail.com>
2017-03-20 16:00:19 -0400

    Added js/main.js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("input[type='checkbox']").on('click', function(){
+    var checked=$(this).is(':checked');
+    var flavor=$(this).val();
+    if (checked){
+      $('td.flavor').each(function(){
+        if($(this).html().toLowerCase().indexOf(flavor)< 0){
+        $(this).parent().hide();
+        }
+      })
+    }
+    else {
+      $('td.flavor').each(function(){
+        if($(this).html().toLowerCase().indexOf(flavor)< 0){
+        $(this).parent().show();
+        }
+      })
+    }
+  });
+
+})
